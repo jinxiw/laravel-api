7b219f8d6c6a704b87b2cc86aac552831bb80c40
wjx <1217429730@qq.com>
2020-07-15 16:46:15 +0800

    发送短信验证码

--- routes/api.php
+++ routes/api.php
@@ -17,10 +17,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('v1')->name('api.v1.')->group(function() {
-    Route::get('version', function() {
-        abort(403, 'test');
-        return 'this is version v1';
-    })->name('version');
+Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function () {
+    // 短信验证码
+    Route::post('verificationCodes', 'VerificationCodesController@store')
+        ->name('verificationCodes.store');
 });
 
