95e99d59cbd5591877a73c3a7e6fbf8fbd306fc1
wjx <1217429730@qq.com>
2020-07-17 16:10:13 +0800

    add socialiteproviders

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use SocialiteProviders\Manager\SocialiteWasCalled;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -21,7 +22,11 @@ class EventServiceProvider extends ServiceProvider
         ],
         Verified::class => [
             \App\Listeners\EmailVerified::class,
-        ]
+        ],
+        SocialiteWasCalled::class => [
+            // add your listeners (aka providers) here
+            'SocialiteProviders\Weixin\WeixinExtendSocialite@handle'
+        ],
     ];
 
     /**
